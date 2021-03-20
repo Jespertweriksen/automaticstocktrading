4a9dc94908036d630ed7e9249ef0ec8533c343c6
nikl1425 <niklas@astne.dk>
2021-03-20 04:26:24 +0100

    czc

---
+++ AutomaticStockTrading/MachineLearning/output.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearning
+{
+    class Class1
+    {
+        public float[] ForecastedRentals { get; set; }
+
+        public float[] LowerBoundRentals { get; set; }
+
+        public float[] UpperBoundRentals { get; set; }
+    }
+}
