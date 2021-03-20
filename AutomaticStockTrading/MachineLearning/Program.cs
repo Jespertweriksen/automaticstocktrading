4bab16a8ae85c11be6a34e6eac5271241e7b3488
Jesper <Jespertweriksen@gmail.com>
2021-03-20 02:52:19 +0100

    changes

--- AutomaticStockTrading/MachineLearning/Program.cs
+++ AutomaticStockTrading/MachineLearning/Program.cs
@@ -11,45 +11,7 @@ namespace MachineLearning
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Test!");
-
-            var engine = Python.CreateEngine();
-
-            // 2) Provide script and arguments
-            var script = @"C:\AllTech\Code\DaysBetweenDates.py";
-            var source = engine.CreateScriptSourceFromFile(script);
-
-            var argv = new List<string>();
-            argv.Add("");
-            argv.Add("2019-1-1");
-            argv.Add("2019-1-22");
-
-            engine.GetSysModule().SetVariable("argv", argv);
-
-            // 3) Output redirect
-            var eIO = engine.Runtime.IO;
-
-            var errors = new MemoryStream();
-            eIO.SetErrorOutput(errors, Encoding.Default);
-
-            var results = new MemoryStream();
-            eIO.SetOutput(results, Encoding.Default);
-
-            // 4) Execute script
-            var scope = engine.CreateScope();
-            source.Execute(scope);
-
-            // 5) Display output
-            string str(byte[] x) => Encoding.Default.GetString(x);
-
-            Console.WriteLine("ERRORS:");
-            Console.WriteLine(str(errors.ToArray()));
-            Console.WriteLine();
-            Console.WriteLine("Results:");
-            Console.WriteLine(str(results.ToArray()));
-
+            kerasnet.TestXOR();
 
         }
     }
---
+++ AutomaticStockTrading/MachineLearning/kerasnet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Keras.Layers;
+using Keras.Models;
+using Numpy;
+
+namespace MachineLearning
+{
+    public class kerasnet
+    {
+       
+            public static void TestXOR()
+            {
+            NDarray x = np.array(new float[,] { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } });
+            NDarray y = np.array(new float[] {0, 1, 1, 0 });
+
+            var model = new Sequential();
+            model.Add(new Dense(32, activation: "relu", input_shape: new Keras.Shape(2)));
+            model.Add(new Dense(64, activation: "relu"));
+            model.Add(new Dense(1, activation: "sigmoid"));
+
+            model.Compile(optimizer: "sgd", loss: "binary_crossentropy", metrics: new string[] { "accuracy" });
+            model.Fit(x, y, batch_size: 2, epochs: 1000, verbose: 1);
+
+            string json = model.ToJson();
+            System.IO.File.WriteAllText("model.json", json);
+            model.SaveWeight("model.h5");
+
+            var loaded_model = Sequential.ModelFromJson(System.IO.File.ReadAllText("model.json"));
+            loaded_model.LoadWeight("model.h5");
+
+        }
+       
+    }
+}
