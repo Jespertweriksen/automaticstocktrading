8480144449bff4e2ecd3eaa5f874dcca7540c5e1
Jesper <Jespertweriksen@gmail.com>
2021-03-20 14:58:05 +0100

    apis

---
+++ AutomaticStockTrading/AutomaticStockTrading/Controllers/StockAPIController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutomaticStockTrading.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AutomaticStockTrading.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<IList<Models.StockModel>> PostStocks(string id)
+        {
+            var list = Tools.GetStocks(id);
+            return Ok(list);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        // GET: api/values
+        [HttpGet("{id}")]
+        public List<Models.StockModel> Get(string id)
+        {
+            return Tools.GetStocks(id);
+        }
+
+    }
+}
