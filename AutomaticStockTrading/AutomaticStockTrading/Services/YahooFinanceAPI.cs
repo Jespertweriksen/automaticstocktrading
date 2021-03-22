bece37316b25060068fd8b090c5571ac090609ba
Niklas Hjort <niklas@astne.dk>
2021-03-22 19:02:40 +0100

    timezone work cross platform

--- AutomaticStockTrading/AutomaticStockTrading/Services/YahooFinanceAPI.cs
+++ AutomaticStockTrading/AutomaticStockTrading/Services/YahooFinanceAPI.cs
@@ -11,6 +11,7 @@ using Newtonsoft.Json;
 using System;
 using System.Runtime.Caching;
 using System.Globalization;
+using TimeZoneConverter;
 
 namespace AutomaticStockTrading.Services
 {
@@ -25,10 +26,10 @@ namespace AutomaticStockTrading.Services
 
         public static List<StockModel> Stocks(string stockAlias)
         {
-            TimeZoneInfo cet = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
-            DateTimeOffset offset = TimeZoneInfo.ConvertTime(DateTime.Now, cet);
+           // TimeZoneInfo cet = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+           // DateTimeOffset offset = TimeZoneInfo.ConvertTime(DateTime.Now, cet);
 
-            Console.WriteLine(offset);
+           // Console.WriteLine(offset);
             
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.twelvedata.com/time_series?symbol=" + stockAlias + "&interval=1day&type=stock&format=JSON&start_date=2001-01-01%2021:24:00&end_date=2021-03-19%2021:24:00&apikey=6a81f55d739d49c2a19610cd4a98e366");
@@ -46,7 +47,7 @@ namespace AutomaticStockTrading.Services
         public static List<StockModel> GetStocks(string stockAlias)
         {
             
-            TimeZoneInfo cet = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+            TimeZoneInfo cet = TZConvert.GetTimeZoneInfo("Central European Standard Time");
             DateTimeOffset offset = TimeZoneInfo.ConvertTime(DateTime.Now, cet);
             
             var cache = MemoryCache.Default;
