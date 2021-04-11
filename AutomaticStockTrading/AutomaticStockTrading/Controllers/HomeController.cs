9c5cd83f184fd225bc86105e083806c93b6a4ed8
jjfiv <jjfivel@gmail.com>
2021-04-11 22:43:38 +0200

    Fixet CSS

--- AutomaticStockTrading/AutomaticStockTrading/Controllers/HomeController.cs
+++ AutomaticStockTrading/AutomaticStockTrading/Controllers/HomeController.cs
@@ -35,6 +35,11 @@ namespace AutomaticStockTrading.Controllers
         {
             return View();
         }
+        
+        public IActionResult Forside()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
