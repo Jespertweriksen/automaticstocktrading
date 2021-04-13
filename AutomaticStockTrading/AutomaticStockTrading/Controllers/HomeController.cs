7d8611d2f14dcf6be55ccc6ede04fd6feb69c98a
jjfiv <jjfivel@gmail.com>
2021-04-13 13:49:38 +0200

    Userpages - initial commit

--- AutomaticStockTrading/AutomaticStockTrading/Controllers/HomeController.cs
+++ AutomaticStockTrading/AutomaticStockTrading/Controllers/HomeController.cs
@@ -13,9 +13,7 @@ namespace AutomaticStockTrading.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-
-
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
---
+++ AutomaticStockTrading/AutomaticStockTrading/Controllers/YourPagesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomaticStockTrading.Controllers
+{
+    public class YourPagesController : Controller
+    {
+        public IActionResult Orders()
+        {
+            return View();
+        }
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
