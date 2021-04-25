59dae599a5162bede8efe97ffa11d21c6f880667
nikl1425 <niklas@astne.dk>
2021-04-25 12:05:11 +0200

    graph work

--- AutomaticStockTrading/AutomaticStockTrading/Services/StockDataService.cs
+++ AutomaticStockTrading/AutomaticStockTrading/Services/StockDataService.cs
@@ -76,11 +76,7 @@ namespace AutomaticStockTrading.Services
                 var stockObject = JsonSerializer.Deserialize<StockTypePriceDto>(response);
                 stockObject.name = stockAlias;
                 cache.Add(stockName, stockObject, expiration);
-
             }
-
-
-
             return cache.Get(stockName, null) as StockTypePriceDto;
         }
 
@@ -98,6 +94,17 @@ namespace AutomaticStockTrading.Services
                 return reader.ReadToEnd();
             }
         }
+
+
+        public List<StockDataModel> GetSpecificTypeData(string stockName)
+        {
+            var query = (from x in context.stockdata
+                         join t in context.stocktype on x.stock_type_id equals t.id
+                         where t.name == stockName
+                         select x).ToList();
+            return query;
+        }
+
     }
     
 }
