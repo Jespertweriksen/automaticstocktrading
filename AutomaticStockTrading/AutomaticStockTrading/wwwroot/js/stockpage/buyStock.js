3194d3da0ab1394d1445cd0d1971dc5edeb7abe1
Niklas Hjort <niklas@astne.dk>
2021-05-01 23:48:37 +0200

    added counter

---
+++ AutomaticStockTrading/AutomaticStockTrading/wwwroot/js/stockpage/buyStock.js
@@ -0,0 +1,36 @@
+﻿
+
+var currentAmount = document.getElementById("currentBuyingAmount");
+var incrementButton = document.getElementById("increment");
+var decreaseButton = document.getElementById("decrease");
+
+var count = 0;
+
+currentAmount.innerHTML = count;
+
+function incrementAmount() {  
+    count++;
+    console.log(count)
+}
+
+function decreaseAmount() {
+    if (count > 0) {
+        count--;
+        console.log(count);
+    }
+    
+}
+
+function updateDisplay() {
+    currentAmount.innerHTML = count;
+}
+
+decreaseButton.onclick = () => {
+    decreaseAmount();
+    updateDisplay();
+}
+
+incrementButton.onclick = () => {
+    incrementAmount();
+    updateDisplay();
+}
